233367d34eace2532c405dc869188894e699c0a8
Blake Chambers <chambersbw@gmail.com>
2024-06-21 00:24:02 -0500

    Add all options to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
 import ai.AIGame;
+import game.TerminalGame;
+import graphics.GameWindow;
 
 public class Main {
     public static void main(String[] args) {
-        //        GameWindow game = new GameWindow();
-        AIGame game = new AIGame();
-        game.run();
+        GameWindow game = new GameWindow();
+
+//        AIGame game = new AIGame();
+//        game.run();
+
+//        TerminalGame game = new TerminalGame();
+//        game.run();
     }
 }
